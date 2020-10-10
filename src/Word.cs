9c4501b120b4a66cbd543b17289959511dab865d
Nic Ott <nicsonfire4@gmail.com>
2020-10-10 17:13:36 -0400

    word definition

---
+++ src/Word.cs
@@ -0,0 +1,21 @@
+class Word
+{
+    /*CONSTANTS*/
+    const int WORD_SIZE = 4;    //word size in bytes
+
+    /*CONSTRUCTORS*/
+    public Word()
+    {
+        data = new byte[WORD_SIZE];
+    }
+
+    /*MEMBERS*/
+    private byte[] data = new byte[WORD_SIZE];
+
+    /*PROPERTIES*/
+    byte[] Data
+    {
+        get { return data; }
+        set { data = value; }
+    }
+}
