c1059176e9677bf37a5ba04bb52e7fde9ace330c
TheNotBotBot <kellenwhill@gmail.com>
2020-10-10 19:09:37 -0400

    added remaining conversion functions

--- src/Utilities.cs
+++ src/Utilities.cs
@@ -13,5 +13,20 @@ public static class Utilities
     {
         return Convert.ToInt32(hex, 16);
     }
-
+    public static string HexToBin(string hex)
+    {
+        return Convert.ToString(Convert.ToInt32(hex, 16), 2);
+    }
+    public static string BinToHex(string bin)
+    {
+        return Convert.ToInt32(bin, 2).ToString("X");
+    }
+    public static int BinToDec(string bin)
+    {
+        return Convert.ToInt32(bin, 2);
+    }
+    public static string DecToBin(int dec)
+    {
+        return Convert.ToString(dec, 2);
+    }
 }
