716514a22a3418b222c643f81460b3ddf8ea5134
Nic Ott <nicsonfire4@gmail.com>
2020-10-10 17:51:16 -0400

    started HexToDec()

---
+++ src/Utilities.cs
@@ -0,0 +1,16 @@
+public static class Utilities
+{
+
+    // Utility functions that will convert instructions to usable data
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="hex"></param>
+    /// <returns></returns>
+    static int HexToDec(string hex)
+    {
+
+    }
+
+}
