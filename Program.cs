86d40fb51e67ccbd084a0add55b3e0a8d4fd5f05
Nic Ott <nicsonfire4@gmail.com>
2020-10-10 16:42:01 -0400

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace os_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
