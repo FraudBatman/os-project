768ffbee97ad6cf61eb657559dc3ef008c084c1a
Nic Ott <nicsonfire4@gmail.com>
2020-10-10 17:56:50 -0400

    finished documentation

--- src/Utilities.cs
+++ src/Utilities.cs
@@ -5,10 +5,10 @@ public static class Utilities
     // Utility functions that will convert instructions to usable data
 
     /// <summary>
-    /// 
+    /// Converts hex as string to decimal
     /// </summary>
-    /// <param name="hex"></param>
-    /// <returns></returns>
+    /// <param name="hex">string of hex value</param>
+    /// <returns>decimal value of hex</returns>
     public static int HexToDec(string hex)
     {
         return Convert.ToInt32(hex, 16);
