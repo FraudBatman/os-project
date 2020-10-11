a90c3f5ce894c1ccc4e9c0e6891e4edecab217b2
Nic Ott <nicsonfire4@gmail.com>
2020-10-10 21:01:33 -0400

    added nicer constructor

--- src/Word.cs
+++ src/Word.cs
@@ -1,3 +1,4 @@
+using System;
 public class Word
 {
     /*CONSTANTS*/
@@ -7,6 +8,21 @@ public class Word
     public Word()
     {
         data = new byte[WORD_SIZE];
+        for (int i = 0; i < WORD_SIZE; i++)
+        {
+            data[i] = 0;
+        }
+    }
+
+    /// <summary>
+    /// Constructor for word straight from byte array, skipping some arbitrary steps
+    /// </summary>
+    /// <param name="input">byte array, should always be the size of the word</param>
+    public Word(byte[] input)
+    {
+        if (input.Length != WORD_SIZE)
+            throw new System.Exception($"Invalid Word size, expected {WORD_SIZE}, was {input.Length}");
+        data = input;
     }
 
     /*MEMBERS*/
