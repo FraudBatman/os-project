f754dc33a03194292a28dea388ea732b9ddfa41e
Nic Ott <nicsonfire4@gmail.com>
2020-12-04 21:19:17 -0500

    Revert "Rewrote RAM. Again."
    This reverts commit b64513f1c115f87604064895ca28b0cb9e774d78.

--- src/RAM.cs
+++ src/RAM.cs
@@ -4,23 +4,100 @@ using System.Text.RegularExpressions;
 
 namespace os_project
 {
-    public static class RAM
+    // Acts as the middle man between the system schedulers and RAM by managing the logical addresses
+    public static class MemoryManager
     {
-        /// <summary>
-        /// Size of RAM in terms of words
-        /// </summary>
-        public const int RAM_SIZE = 1024;
+        private static RAM _RAM = new RAM();
 
-        public static Word[] data = new Word[RAM_SIZE];
-
-        public static Word Read(int index)
+        public static bool IsFull()
         {
-            return data[index];
+            return _RAM.AllocatedSpace() == 0 ? false : true;
+        }
+    }
+
+    // Just a class container that has a bank of register objects stored in an array
+    // No class has acess to registers other than RAM 
+    public class RAM
+    {
+        public const int RAM_SIZE = 1024;
+        Register[] PHYSICAL_RAM = new Register[RAM_SIZE];
+
+        // Creates a bank of registers for the memory
+        public RAM()
+        {
+            for (int i = 0; i < PHYSICAL_RAM.Length; i++)
+            {
+                PHYSICAL_RAM[i] = new Register(i);
+            }
         }
 
-        public static void Write(int index, Word value)
+        // Read the instruction at the physical address in the register
+        public string ReadFromRegister(int p_address)
         {
-            data[index] = value;
+            return PHYSICAL_RAM[p_address].Instruction.Value;
+        }
+
+        // Write the instruction to the register in RAM at the register
+        public void WriteToRegister(int p_address, Word word)
+        {
+            PHYSICAL_RAM[p_address].Register_Owner = word.OwnerOfWord;
+            PHYSICAL_RAM[p_address].Instruction = word;
+        }
+
+        public int AllocatedSpace()
+        {
+            var count = 0;
+            foreach (var reg in PHYSICAL_RAM)
+            {
+                if (reg.Instruction != null)
+                    count++;
+            }
+
+            System.Console.WriteLine(count);
+            return count;
+        }
+
+        public int EmptySpace()
+        {
+            var count = 0;
+            foreach (var reg in PHYSICAL_RAM)
+            {
+                if (reg.Instruction == null)
+                    count++;
+            }
+            return count;
+        }
+
+        private class Register
+        {
+            private int? reg_owner;
+            private int p_address;
+            private Word word;
+
+            public Register(int physical_address, int? reg_owner = null)
+            {
+                this.reg_owner = reg_owner;
+                this.p_address = physical_address;
+                this.word = null;
+            }
+
+            public int? Register_Owner
+            {
+                get { return this.reg_owner; }
+                set { this.reg_owner = value; }
+            }
+
+            public int P_Address
+            {
+                get { return this.p_address; }
+                set { this.p_address = value; }
+            }
+
+            public Word Instruction
+            {
+                get { return this.word; }
+                set { this.word = value; }
+            }
         }
     }
 }
